dba8e7008564fc35543f7b85545b167555325f6a
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-05 15:49:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ drivers = ['steve', 'ryan', 'dillan', 'dustin', 'jody', 'zach', 'brandon']
 function findMatching(drivers, name) { 
   return drivers.filter(function driverName) { 
     return driverName.toLowerCase() === name.toLowerCase();
-  })
+  });
 } 
 
 function fuzzyMatch() { 
