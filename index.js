0bbbddda7db74f18af96a951188ca9684225171f
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-05 15:49:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 drivers = ['steve', 'ryan', 'dillan', 'dustin', 'jody', 'zach', 'brandon']
 
 function findMatching(drivers, name) { 
-  return drivers.filter(function driverName)
-    return
+  return drivers.filter(function driverName) { 
+    return driverName.toLowerCase 
   })
 } 
 
